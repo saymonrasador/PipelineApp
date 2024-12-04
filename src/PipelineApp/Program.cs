809fb0b4131bbeab8f5f7472f20161340bb47645
saymonrasador <srrasador@ucs.br>
2024-12-04 14:02:44 -0300

    Update Program.cs

--- src/PipelineApp/Program.cs
+++ src/PipelineApp/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Program.cs atualmente não faz nada além de exibir "Hello, World!", ele não é obrigatório para o funcionamento da aplicação como um todo. No entanto, pode ser útil para:
+//Testar funcionalidades específicas localmente (como comandos para gerenciar o banco de dados, fazer migrações, etc.).
+//Executar scripts ou tarefas administrativas.
+
 Console.WriteLine("Hello, World!");
